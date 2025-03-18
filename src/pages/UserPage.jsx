bdfae5534addb955b3a653bdcd8ef506c01595ec
Morad17 <morad123@hotmail.co.uk>
2025-03-18 00:09:36 +0000

    forming template for user page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -13,7 +13,28 @@ const checkUser = () => {
 
 
   return (
-    <div>UserPage</div>
+    <div className="user-page-section">
+      <div className="top-row">
+        <section className="liked-section">
+          <p>Test</p>
+        </section>
+        <section className="watch-list-section">
+        </section>
+      </div>
+      <div className="middle-row">
+        <section className="user-overview-section">
+        </section>
+      </div>
+      
+      <div className="bottom-row">
+        <section className="reviewed-movies-section">
+        </section> 
+        <section className="stats-section">
+        </section>
+      </div>
+
+     
+    </div>
   )
 }
 
