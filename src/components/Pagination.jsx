22b4e77b88d73101b4c4f4ab76a09835e24b35a8
Morad17 <morad123@hotmail.co.uk>
2025-04-18 11:36:34 +0100

    updates

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Pagination = ({paginate, cardsPerPage, currentPage}) => {
+    const pageNumbers = []
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Pagination
--- src/pages/MovieLibrary.jsx
+++ src/pages/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react'
 import MovieCard from '../components/MovieCard';
 import { ToastContainer, toast } from "react-toastify";
 import axios from 'axios';
+import Pagination from '../components/Pagination';
 
 
 const MovieLibrary = () => {
@@ -9,9 +10,9 @@ const MovieLibrary = () => {
 ////Use States////
 const [movieData, setMovieData ] = useState([])
 const [genres, setGenres ] = useState([])
-const [userMovieData, setUserMovieData ] = useState({})
+const [userMovieData, setUserMovieData ] = useState([])
 //Pagination
-const [currentPage,getCurrentPage ] = useState(1)
+const [currentPage,setCurrentPage ] = useState(1)
 const [cardsPerPage, setCardsPerPage] = useState(21)
 
 // Get All Movies//
@@ -23,10 +24,11 @@ const fetchMovies = async () => {
       Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_TOKEN}`
     }
   };
-  const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US'
   try{
     const res = await fetch(url , options)
     const data = await res.json()
+    console.log(data)
     let arr = []
     data.results.forEach ( movie =>{
       arr.push(movie)
@@ -82,16 +84,18 @@ useEffect(() => {
 //Pagination
 const indexOfLastCard = currentPage * cardsPerPage
 const indexOfFirstCard = indexOfLastCard - cardsPerPage
-// const currentCards 
+const currentCards  = movieData.slice(indexOfFirstCard,indexOfLastCard)
+const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
 
   return (
     <div className="movie-library-section">
-      { movieData && movieData.map((movie, key)=> {
+      { currentCards && currentCards.map((movie, key)=> {
         const userMovie = userMovieData?.filter((userMovie)=> userMovie.movieName === movie.title)[0] || null
         return <MovieCard userData={userMovie} key={key} movie={movie} genres={genres}/>
         })
       }
+      <Pagination paginate={paginate} cardsPerPage currentPage />
     </div>
   )
 }
