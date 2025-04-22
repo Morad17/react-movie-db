2e414be9272a96e0a33f0cf1f5295509d14d8b92
Morad17 <morad123@hotmail.co.uk>
2025-04-22 08:10:24 +0100

    search updates

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -6,11 +6,34 @@ const SearchMovie = () => {
     useEffect(()=>{},[])
 
     const [searchQuery, setSearchQuery] = useState('')
+    const [searchRes, setSearchRes] = useState('')
 
+    const searchFunction = async () => {
+        const user = localStorage.getItem("username")
+        if (user) {
+            const options = {
+                method: 'GET',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_TOKEN}`
+                }
+              };
+              const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}include_adult=false&language=en-US&page=1`
+              try{
+                const res = await fetch(url , options)
+                const data = await res.json()
+                setSearchRes(data)
+                console.log(searchQuery,data)
+              } catch (err) {
+                console.log(err)
+              }
+          }
+       
+      }
 
     return (
         <div className="search-movie">
-            <input className="search-input" type="text" value={searchQuery} onChange={()=> setSearchQuery()} />
+            <input className="search-input" type="text" value={searchQuery} onChange={(e)=>  setSearchQuery(e.target.value)} />
             <button onClick={()=> {searchFunction()}} className="search-btn" type="submit">Search</button>
         </div>
     )}
