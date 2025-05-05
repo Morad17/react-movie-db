27d5105adc6db550c31bf64eca8176c06d29fe30
Morad17 <morad123@hotmail.co.uk>
2025-05-05 22:33:32 +0100

    creating movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 
+import placeholder from '../assets/scss/images/poster-placeholder.png'
+import { GiRoundStar } from 'react-icons/gi'
+
 const MoviePage = () => {
 
     const [movieInfo, setMovieInfo] = useState()
 
     const params = useParams()
     const getMoveInfo = async () => {
-        const id = params
+        const { id } = params
         const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`
-        console.log(url)
         const options = {
             method: 'GET',
             headers: {
@@ -27,10 +29,40 @@ const MoviePage = () => {
     }
     useEffect(()=> {    
         getMoveInfo()
-        console.log(movieInfo)
     },[])
-  return (
-    <div>MoviePage</div>
+  return ( 
+    <div className="movie-page-section">
+        {movieInfo ? 
+            <div className="movie-page-card">
+                <img src={movieInfo.poster_path ? `https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`: placeholder}  alt="" className="poster-image" />
+                <div className="movie-page-info-section">
+                <h2>{movieInfo.title} {movieInfo.release_date.slice(0,4)} {movieInfo.origin_country}</h2>
+                <p className="vote-number"><GiRoundStar />{movieInfo.vote_average?.toFixed(1)}</p>
+                <div className="genre-section">
+                    {
+                    movieInfo.genres.map((g,key) => {
+                        return <p className="genre" key={key}>{g.name}</p>
+                    }) 
+                    }
+                    
+                </div>
+                </div>
+                <div className="overview-section">
+                    <p className="overview">{movieInfo.overview}</p>
+                </div>
+                <div className="cast-section">
+
+                </div>
+                <div className="trailer-section">
+
+                </div>
+                <div className="reviews-section">
+
+                </div> 
+            </div>: 
+            <p>Loading...</p>}
+    </div>
+        
   )
 }
 
