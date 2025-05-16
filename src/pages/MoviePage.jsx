672a5fb2d056b1cf8317345121779d363b35c892
Morad17 <morad123@hotmail.co.uk>
2025-05-16 12:16:45 +0100

    fixed fade for movie-page banner/card

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -80,7 +80,7 @@ const MoviePage = () => {
     if (!results) return "N/A"; // Return 'N/A' if results are not available
 
     const gbRelease = results.find((release) => release.iso_3166_1 === "GB"); // Find the release for 'GB'
-    if (!gbRelease) return "N/A"; // Return 'N/A' if no 'GB' release is found
+    if (!gbRelease) return ""; // Return 'N/A' if no 'GB' release is found
 
     const certification = gbRelease.release_dates?.[0]?.certification; // Get the certification
     switch (certification) {
@@ -101,96 +101,109 @@ const MoviePage = () => {
     <div className="movie-page-section">
       <div className="movie-section-left">{console.log(selectedMovieInfo)}</div>
       {selectedMovieInfo ? (
-        <div
-          className="movie-page-card"
-          style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${selectedMovieInfo.backdrop_path})`,
-          }}
-        >
-          <div className="backdrop-wrapper"></div>
-          <section className="top-content">
-            <div className="poster-image-div">
-              <img
-                src={
-                  selectedMovieInfo?.poster_path
-                    ? `https://image.tmdb.org/t/p/w500/${selectedMovieInfo.poster_path}`
-                    : placeholder
-                }
-                alt=""
-                className="poster-image"
-              />
-            </div>
-            <div className="movie-page-info-section">
-              <h2 className="movie-info-title">
-                {selectedMovieInfo.title} (
-                {selectedMovieInfo.release_date.slice(0, 4)})
-                <ReactCountryFlag
-                  countryCode={`${selectedMovieInfo.origin_country[0]}`}
-                  svg
-                  className="react-flag-icon"
+        <div className="movie-section-center">
+          <section
+            className="movie-page-card"
+            style={{
+              "--movie-bg-image": `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${selectedMovieInfo.backdrop_path})`,
+            }}
+          >
+            <div className="backdrop-wrapper"></div>
+            <div className="top-content">
+              <div className="poster-image-div">
+                <img
+                  src={
+                    selectedMovieInfo?.poster_path
+                      ? `https://image.tmdb.org/t/p/w500/${selectedMovieInfo.poster_path}`
+                      : placeholder
+                  }
+                  alt=""
+                  className="poster-image"
                 />
-              </h2>
-              <div className="info-icons-row">
-                {selectedMovieInfo.genres.map((g, key) => {
-                  return (
-                    <p className="movie-genre" key={key}>
-                      {g.name}
-                    </p>
-                  );
-                })}
-                <p className="age-rating">{ageRating()}</p>
+              </div>
+              <div className="movie-page-info-section">
+                <h2 className="movie-info-title">
+                  {selectedMovieInfo.title} (
+                  {selectedMovieInfo.release_date.slice(0, 4)})
+                  <ReactCountryFlag
+                    countryCode={`${selectedMovieInfo.origin_country[0]}`}
+                    svg
+                    className="react-flag-icon"
+                  />
+                </h2>
+                <div className="info-icons-row">
+                  {selectedMovieInfo.genres.map((g, key) => {
+                    return (
+                      <p className="movie-genre" key={key}>
+                        {g.name}
+                      </p>
+                    );
+                  })}
+                  <p className="age-rating">{ageRating()}</p>
 
-                <p className="vote-number">
-                  <GiRoundStar />
-                  {selectedMovieInfo.vote_average?.toFixed(1)}
-                </p>
+                  <p className="vote-number">
+                    <GiRoundStar />
+                    {selectedMovieInfo.vote_average?.toFixed(1)}
+                  </p>
+                </div>
+                <div className="action-icons-row">
+                  <BsBookmarkStarFill
+                    id="bookmarkIcon"
+                    className={`save-svg ${
+                      userActions.bookmarkList ? "bookmarked" : ""
+                    }`}
+                    onClick={() =>
+                      !isDisabled &&
+                      addToBookmarkList({
+                        id: selectedMovieInfo.id,
+                        title: selectedMovieInfo.title,
+                        username: username,
+                        poster_path: selectedMovieInfo.poster_path,
+                        userActions,
+                        setUserActions,
+                      })
+                    }
+                  />
+                  <BiHeartCircle
+                    id="likeIcon"
+                    className={`heart-svg ${
+                      userActions.likedList ? "liked" : ""
+                    }`}
+                    onClick={() =>
+                      !isDisabled &&
+                      addToLikedList({
+                        id: selectedMovieInfo.id,
+                        title: selectedMovieInfo.title,
+                        username: username,
+                        poster_path: selectedMovieInfo.poster_path,
+                        userActions: userActions,
+                        setUserActions: setUserActions,
+                      })
+                    }
+                  />
+                </div>
+                <div className="overview-section">
+                  <h3 className="section-title">Overview</h3>
+                  <p className="overview-text">{selectedMovieInfo.overview}</p>
+                </div>
               </div>
-              <div className="action-icons-row">
-                <BsBookmarkStarFill
-                  id="bookmarkIcon"
-                  className={`save-svg ${
-                    userActions.bookmarkList ? "bookmarked" : ""
-                  }`}
-                  onClick={() =>
-                    !isDisabled &&
-                    addToBookmarkList({
-                      id: selectedMovieInfo.id,
-                      title: selectedMovieInfo.title,
-                      username: username,
-                      poster_path: selectedMovieInfo.poster_path,
-                      userActions,
-                      setUserActions,
-                    })
-                  }
-                />
-                <BiHeartCircle
-                  id="likeIcon"
-                  className={`heart-svg ${
-                    userActions.likedList ? "liked" : ""
-                  }`}
-                  onClick={() =>
-                    !isDisabled &&
-                    addToLikedList({
-                      id: selectedMovieInfo.id,
-                      title: selectedMovieInfo.title,
-                      username: username,
-                      poster_path: selectedMovieInfo.poster_path,
-                      userActions: userActions,
-                      setUserActions: setUserActions,
-                    })
-                  }
-                />
-              </div>
-              <div className="overview-section">
-                <h3 className="section-title">Overview</h3>
-                <p className="overview-text">{selectedMovieInfo.overview}</p>
-              </div>
+            </div>
+
+            <div className="cast-section"></div>
+            <div className="trailer-section"></div>
+            <div className="reviews-section"></div>
+          </section>
+          <section className="movie-page-content">
+            <div className="trailer-section">
+              <h3 className="section-title">Trailer</h3>
+            </div>
+            <div className="cast-section">
+              <h3 className="section-title">Top Cast</h3>
+            </div>
+            <div className="reviews-ratings-section">
+              <h3 className="section-title">Reviews & Ratings</h3>
             </div>
           </section>
-
-          <div className="cast-section"></div>
-          <div className="trailer-section"></div>
-          <div className="reviews-section"></div>
         </div>
       ) : (
         <p>Loading...</p>
