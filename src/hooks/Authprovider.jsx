082b0e3e9b976fe0d985bbe77b9b1b9f2ab4531c
Morad17 <morad123@hotmail.co.uk>
2025-06-17 16:46:47 +0100

    fixed userpage issue

--- src/hooks/Authprovider.jsx
+++ src/hooks/Authprovider.jsx
@@ -5,10 +5,7 @@ import { useNavigate } from "react-router";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    username: null,
-    profileImage: null,
-  });
+  const [user, setUser] = useState(null);
   const [token, setToken] = useState("");
   const navigate = useNavigate();
 
